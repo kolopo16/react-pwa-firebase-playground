ff58750d9933fbdf2e0de3f81a35b6d2e4a26d1a
kolopo16 <unlimited.g001@gmail.com>
2017-06-11 10:15:54 +0700

    Add project to git

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Header from './header';
+import firebaseConfig from './firebaseConfig';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message : []
+    }
+  }
+
+  componentWillMount() {
+      const database = firebaseConfig().database().ref("/");
+      database.on('child_added', snapshot => {
+        let message = {
+          val: snapshot.val(),
+          id: snapshot.key
+        };
+        this.setState({
+          message : [message].concat(this.state.message)
+        });
+      })
+  }
+
+  renderMessage() {
+    let rows = this.state.message.map(message => {
+      return <div className="tweet-block" key={message.id}>
+                <div className="profile-picture">
+                  {/* <img src={message.val.user.profile_image_url_https} alt="รูปภาพ" /> */}
+                </div>
+                <div className="tweet-message">
+                  {message.val.text}
+                </div>
+              </div>
+    })
+
+    return rows
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="twitter">
+          { this.renderMessage() }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="tweet-header">
+        Kool Tweet
+      </div>
+    );
+  }
+}
+
+export default Header;
